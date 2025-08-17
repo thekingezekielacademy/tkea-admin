9edcb3b0199818245fba85eea7512f49c88ed297
olasunkanmi <119403527+ColourfulRhythm@users.noreply.github.com>
2025-08-17 11:10:39 +0100

    Trigger Vercel deployment for ScrollToTop component

--- client/src/components/ScrollToTop.tsx
+++ client/src/components/ScrollToTop.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
+// Enhanced ScrollToTop component for mobile and desktop navigation
 const ScrollToTop: React.FC = () => {
   const { pathname } = useLocation();
 
