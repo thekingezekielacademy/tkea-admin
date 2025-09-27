b4edd37b8fd9e6813fed986d9b79a3e013a9bbe9
thekingezekielacademy <thekingezekielacademy@gmail.com>
2025-09-27 19:52:35 +0100

    Add Facebook Pixel ID 1991898708291767 to layout and update tracking functions

--- king-ezekiel-academy-nextjs/src/app/layout.tsx
+++ king-ezekiel-academy-nextjs/src/app/layout.tsx
@@ -35,6 +35,29 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Facebook Pixel */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1991898708291767');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img height="1" width="1" style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=1991898708291767&ev=PageView&noscript=1"
+          />
+        </noscript>
+        
         {/* Optimized font loading - only preconnect, no preload */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
--- king-ezekiel-academy-nextjs/src/hooks/useFacebookPixel.ts
+++ king-ezekiel-academy-nextjs/src/hooks/useFacebookPixel.ts
@@ -2,35 +2,37 @@ import { useEffect } from 'react';
 
 export const useFacebookPixel = () => {
   useEffect(() => {
-    // Facebook Pixel initialization would go here
-    // For now, we'll just provide the interface
+    // Facebook Pixel is initialized in layout.tsx
   }, []);
 
   const trackContact = (eventName: string, data: any) => {
-    if (typeof window !== 'undefined') {
-      console.log('Facebook Pixel - Contact Event:', eventName, data);
-      // Actual Facebook Pixel tracking would go here
+    if (typeof window !== 'undefined' && (window as any).fbq) {
+      (window as any).fbq('track', 'Contact', data);
     }
   };
 
   const trackLead = (eventName: string, value: number, data: any) => {
-    if (typeof window !== 'undefined') {
-      console.log('Facebook Pixel - Lead Event:', eventName, value, data);
-      // Actual Facebook Pixel tracking would go here
+    if (typeof window !== 'undefined' && (window as any).fbq) {
+      (window as any).fbq('track', 'Lead', { value, currency: 'NGN', ...data });
     }
   };
 
   const trackCourseView = (courseId: string, courseTitle: string) => {
-    if (typeof window !== 'undefined') {
-      console.log('Facebook Pixel - Course View Event:', courseId, courseTitle);
-      // Actual Facebook Pixel tracking would go here
+    if (typeof window !== 'undefined' && (window as any).fbq) {
+      (window as any).fbq('track', 'ViewContent', {
+        content_type: 'course',
+        content_ids: [courseId],
+        content_name: courseTitle
+      });
     }
   };
 
   const trackSearch = (searchTerm: string, resultsCount: number) => {
-    if (typeof window !== 'undefined') {
-      console.log('Facebook Pixel - Search Event:', searchTerm, resultsCount);
-      // Actual Facebook Pixel tracking would go here
+    if (typeof window !== 'undefined' && (window as any).fbq) {
+      (window as any).fbq('track', 'Search', {
+        search_string: searchTerm,
+        content_category: 'courses'
+      });
     }
   };
 
